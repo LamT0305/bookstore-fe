8c992701ba5695cca99c78bef7f2c63f331b294a
LamT0305 <Lam45883@gmail.com>
2023-10-14 10:16:06 +0700

    Create form login

--- src/components/auth/Login/Login.tsx
+++ src/components/auth/Login/Login.tsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import "./style.css";
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  return (
+    <div className=" login100">
+      <div className="wrap-login">
+        <p className="head-lg">Login</p>
+        <form action="" className="form-login">
+          <div className="input-field">
+            <label htmlFor="user-name">Username</label>
+            <input
+              type="text"
+              id="user-name"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="enter your password"
+            />
+          </div>
+          <p style={{ textAlign: "right" }}>Forgot password?</p>
+          <button>LogIn</button>
+        </form>
+        <p style={{marginTop:50}}> Or sign up</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;

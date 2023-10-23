fb2b8995f21c0338cc398e5bbb2c4f554c3af4ac
PhongHoang68 <phonghnggch210556@fpt.edu.vn>
2023-10-23 10:52:10 +0700

    set up manager systerm

---
+++ src/components/Managebook/Managebook.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+function Managebook() {
+  return (
+    <div>Managebook</div>
+  )
+}
+
+export default Managebook
---
+++ src/pages/Manager.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Managebook from '../components/Managebook/Managebook'
+
+function Manager() {
+  return (
+    <div>
+        <Managebook/>
+    </div>
+  )
+}
+
+export default Manager

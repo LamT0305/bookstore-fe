565b85123cfaa69885b2512514572edfb353f172
lamba <lam45883@gmail.com>
2023-10-26 09:18:32 +0700

    Set up cart user

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -4,7 +4,7 @@ import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
   // baseURL: "https://localhost:7105/api/v1",
-  baseURL: "https://bookstore-api-demo.azurewebsites.net/api/v1", // Replace this with your API base URL
+  baseURL: "https://bookstore-apis-demo.azurewebsites.net//api/v1", // Replace this with your API base URL
   timeout: 10000, // Specify the timeout (optional)
   headers: {
     "Content-Type": "application/json",
